5119b5efe5c1fae5f000f89fbba812a0e31ed98f
chappy31415 <takizawa@usoft,co.jp>
2023-10-08 16:59:47 +0900

    test

--- src/Thanks.js
+++ src/Thanks.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import './styles.css';
+import Mars4 from "./static/mars4.png"
 
 const Thanks = () => {
   return (
     <>
       <h1>購入ありがとうございました。</h1>
       pdfを挿入
+      <img src={Mars4} alt="" width="800px" height="800px" />
       <div>
         <Link to={`/`}>ホームに戻る</Link>
       </div>
