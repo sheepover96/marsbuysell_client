b63363969bb2c33fb9bcfed7dce01d9f128f0dfc
mo-shimada <m-shimada@tsh-world.co.jp>
2023-10-09 12:48:55 +0900

    項目追加

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,7 +2,6 @@ import VastitasBorealis from '../static/lands/vastitas_borealis.jpg'
 import HellasPlanitia from '../static/lands/hellas_planitia.jpg'
 import NoachisTerra from '../static/lands/noachis_terra.jpg'
 import ArabiaTera from '../static/lands/arabia-tera.jpg'
-
 import SyrtisMajor from '../static/lands/syrtis_major.jpg'
 import UtopiaPlanitia from '../static/lands/utopia_planitia.jpg'
 import ElysiumMons from '../static/lands/elysium_mons.jpg'
@@ -22,63 +21,63 @@ export const MARS_LAND_LIST = {
     name: 'アラビア・テラ',
     address: '火星1-1-1',
     price: 10000,
-    description: '太陽系最大の衝突盆地',
+    description: '太陽系最大の衝突盆地。クレーターも少なく住みやすい広大な土地が広がっています。\n空港や都心部へのアクセスが便利です。',
     image: ArabiaTera,
   },
   3: {
     name: 'シルティス・メジャー山',
     address: '火星1-1-1',
     price: 10000,
-    description: '楯状火山\n砂が少なく、玄武岩質の火山岩がむき出しなので黒い。\n★地盤はしっかりしてるから開発向き？',
+    description: '楯状火山のため傾斜が緩やかです。\n玄武岩質の火山岩で形成された固い地盤のため、地震災害の危険性が最も低いと言われています。',
     image: SyrtisMajor,
   },
   4: {
     name: 'ユートピア平原',
     address: '火星1-1-1',
     price: 10000,
-    description: '岩が多く、開発が大変そう\n永久凍土の劣化による地面のくぼみ、スカラップ地形\n★住みにくそう。。',
+    description: '永久凍土の劣化による地面のくぼみや大きな岩が多い。都市緑化計画が進行中です。',
     image: UtopiaPlanitia,
   },
   5: {
     name: 'エリジウム山',
     address: '火星1-1-1',
     price: 10000,
-    description: '火星で２番目に大きな火山',
+    description: '火星で２番目に大きな火山。火星内外からの観光客が多い人気の観光スポット。',
     image: ElysiumMons,
   },
   6: {
     name: 'ティレナ・テラ',
     address: '火星1-1-1',
     price: 10000,
-    description: '赤道の南にある広い地域。\nクレーターの多い高地やその他の険しい地形あり。\n火星で最も古い火山、ティレナパテラ火山がある\nハーシェルクレーター\nバルガン砂丘\n★住みにくそうだけど、観光地沢山、人気ありそう',
+    description: '赤道の南に位置し、クレーターの多い高地や険しい地形もあるため、居住には向かない土地も多いです。\n火星で最も古い火山のティレナパテラ山があります。',
     image: TyrrhenaTerra,
   },
   7: {
     name: 'エリジウム平原',
     address: '火星1-1-1',
     price: 10000,
-    description: 'エリジウム山の南に位置する。\n現在から2万年以内の溶岩流が発見された地域がある(ケルベロス・フォッセ)\n火山、クレーターや川の谷あり\n★火星で一番直近で火山活動が確認されているため危険？',
+    description: '高層ビルからは北部にエリジウム山が望めます。\n2万年以内の溶岩流が発見されたことから、火星最大の火山災害シェルターがあります。',
     image: ElysiumPlanitia
   },
   8: {
     name: 'ノアキス・テラ',
     address: '火星1-1-1',
     price: 10000,
-    description: '広大な南の陸地\n線状の砂丘がある\n★線状を活かす？農業？',
+    description: '広大な南の陸地で、特徴的な線状の砂丘を活かした農業が盛んです。\n特産品のメロンは人気が高く他の惑星へ輸出されるほど。',
     image: NoachisTerra,
   },
   9: {
     name: 'ヘラス平原',
     address: '火星1-1-1',
     price: 10000,
-    description: '巨大な円形のクレーター、太陽系で３または４番目に大きい\nユートピア平原と同じくらいの大きさ',
+    description: '太陽系で３番目に大きな巨大クレーターがあります。\nクレーター内はショッピングモールなど娯楽施設が充実。夢に見たクレーターライフが叶います。',
     image: HellasPlanitia,
   },
   10: {
     name: 'へスペリア平原',
     address: '火星1-1-1',
     price: 10000,
-    description: '広い溶岩平原。平野には滑らかで平らな浅いクレーターが豊富\n湿地もあり',
+    description: '浅いクレーターが多く、滑らかで平らな溶岩平原です。\n緑豊かな湿地には未確認の生物がいる噂も。湿地は火星条約により保護されています。',
     image: HesperiaPlanum
   },
 }
